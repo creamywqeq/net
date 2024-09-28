39f5468d2ceab3dc974c3f82e9a84670da02cc37
creamywqeq <2067732712@qq.com>
2024-09-29 01:40:43 +0800

    11

--- joy-personal-portfolio-main/lib/data.ts
+++ joy-personal-portfolio-main/lib/data.ts
@@ -2,7 +2,6 @@ import React from "react";
 import { FaJava, FaReact } from "react-icons/fa";
 import { FaVuejs } from "react-icons/fa";
 import { LuActivity, LuGraduationCap } from "react-icons/lu";
-import moonImage from '@/public/moon.JPG';
 import moonImage from '@/public/moon.jpg';
 import breadditImage from "@/public/breaddit.png";
 import gameHubImage from "@/public/game-hub.png";
